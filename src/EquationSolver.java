d68423a42d72e0f9e91d214d8a5057c78b24c0ac
khizara555 <khizarcs123@gmail.com>
2022-11-29 23:35:00 +0500

    Generates random array of size n and applies Siedal and Jacobi Methods to it.

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -4,29 +4,31 @@ public class EquationSolver {
 
     private double[][] Matrix;
     private double[] Solution;
+    private double Tolerace = 1 * Math.pow(10, -10);
 
-    public EquationSolver(double[][] Matrix, double[] Solution) {
-        this.Matrix = Matrix;
-        this.Solution = Solution;
+    public EquationSolver(int n) {
+        double sum = 0;
+        Matrix = new double[n][n];
+        Solution = new double[n];
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                Matrix[i][j] = Math.random()*1000;
+                sum = sum + Matrix[i][j];
+            }
+            Matrix[i][i] = sum;
+            Solution[i] = Math.random()*1000;
+            sum = 0;
+        }
     }
-    // public void makeDiagonallyDominant() {
-    // int sum = 0;
-    // for (int i=0; i<Matrix.length; i++) {
-    // for (int j=0; j<Matrix[0].length; j++) {
-    // sum = sum + Matrix[i][j];
-    // }
-    // Matrix[i][i] = sum;
-    // sum = 0;
-    // }
-    // }
 
     public void JacobiIterativeMethod() {
         double[] prev = new double[Matrix.length];
         double[] next = new double[Matrix.length];
-        int maxiterations = 3;
+        double residual = Double.MAX_VALUE;
         int iterations = 0;
         double sum = 0;
-        while (iterations < maxiterations) {
+        while (residual > Tolerace) {
             for (int i = 0; i < Matrix.length; i++) {
                 for (int j = 0; j < Matrix[0].length; j++) {
                     if (i != j) {
@@ -38,17 +40,20 @@ public class EquationSolver {
             }
             prev = next;
             iterations++;
+            double[] Difference = SubtractMatrix(MultiplyMatrix(Matrix, prev), Solution);
+            residual = residual(Difference);
         }
-        System.out.println(Arrays.toString(next));
+        System.out.println("Number of iterations taken for Jacobi: "+iterations);
+        // System.out.println(Arrays.toString(next));
 
     }
 
     public void GaussSiedalMethod() {
         double[] variablevalues = new double[Matrix.length];
-        int maxiterations = 3;
+        double residual = Double.MAX_VALUE;
         int iterations = 0;
         double sum = 0;
-        while (iterations < maxiterations) {
+        while (residual > Tolerace) {
             for (int i=0; i<Matrix.length; i++) {
                 for (int j=0; j<Matrix[0].length; j++) {
                     if (i != j) {
@@ -59,9 +64,44 @@ public class EquationSolver {
                 sum = 0; 
             }
             iterations++;
+            double[] Difference = SubtractMatrix(MultiplyMatrix(Matrix, variablevalues), Solution);
+            residual = residual(Difference);
             
         }
-        System.out.println(Arrays.toString(variablevalues)); 
+        System.out.println("Number of iterations taken for Seidal: "+iterations);
+        // System.out.println(Arrays.toString(variablevalues)); 
+    }
+ 
+    public double[] MultiplyMatrix(double[][] A, double[] x) {
+        double[] C = new double[A.length];
+        
+        for (int i=0; i<A.length; i++) {
+            for (int j=0; j<A[0].length; j++) {
+                C[i] = C[i] + (A[i][j] * x[j]);
+            }
+        }
+
+        return C;
+    }
+
+    public double[] SubtractMatrix(double[] Ax, double[] b) {
+        double[] C = new double[Ax.length];
+
+        for (int i=0; i<Ax.length; i++) {
+            C[i] = Ax[i] - b[i];
+        }
+
+        return C;
+    }
+
+    public double residual(double[] a) {
+        double sum = 0;
+        for (int i=0; i<a.length; i++) {
+            sum = sum + (a[i]*a[i]);
+        }
+        double residual = Math.pow(sum, 0.5);
+        return residual;
+
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
 public class main {
 
     public static void main(String[] args) {
         double [][] a = { {4, 3, 1}, {5,7,1}, {2,-1,9} };
         double [] solution = {9,11,10};
-        EquationSolver d = new EquationSolver(a, solution);
-        // d.JacobiIterativeMethod();
+
+
+        EquationSolver d = new EquationSolver(100);
+        d.JacobiIterativeMethod();
         d.GaussSiedalMethod();
             
     }
